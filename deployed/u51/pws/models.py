aac905851b4fab11bb8fac13aa276d3b0a54f2c7
Niklas Hambüchen <nh2@deditus.de>
2010-03-12 23:14:15 +0100

    Added Linux Libertine font, form updates

--- deployed/u51/pws/models.py
+++ deployed/u51/pws/models.py
@@ -4,7 +4,7 @@ from django.forms import ModelForm
 class Eintrag(models.Model):
 	name = models.CharField(max_length=200)
 	user = models.CharField(max_length=200)
-	pw = models.CharField(max_length=200)
+	pw = models.CharField('Passwort', max_length=200)
 	email = models.EmailField()
 	extra = models.TextField(blank=True)
 	eingetragen = models.DateField(auto_now=True)
--- deployed/u51/pws/views.py
+++ deployed/u51/pws/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import get_object_or_404, render_to_response
 from django.http import HttpResponseRedirect
+from django.views.generic.simple import direct_to_template
 from django.core.urlresolvers import reverse
 from u51.pws.models import Eintrag, EintragForm
 
 def create_edit_eintrag(request, id=None):
-	form = EintragForm(request.POST or None, instance=(id and Eintrag.objects.get(id=id)))
+	form = EintragForm(request.POST or None, instance=(id and Eintrag.objects.get(id=id)), label_suffix='')
 
 	if request.method == 'POST' and form.is_valid():
 		form.save()
 		return HttpResponseRedirect(reverse('liste'))
 
-	return render_to_response('pws/create_edit_eintrag.html', {'form': form})
+	return direct_to_template(request, 'pws/create_edit_eintrag.html', {'form': form})
