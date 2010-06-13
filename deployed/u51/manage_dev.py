2d428e053e7db245c84f7be842d0bcb9ac87b3f7
Niklas Hambüchen <nh2@deditus.de>
2010-06-13 14:14:21 +0200

    Cleanup

--- deployed/u51/manage_dev.py
+++ deployed/u51/manage_dev.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
-from django.core.management import execute_manager
-
 try:
 	import settings_dev # Assumed to be in the same directory.
 except ImportError:
 	import sys
-	sys.stderr.write("Error: Can't find the file 'settings_dev.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings_dev.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
+	sys.stderr.write("Error: Can't find the file 'settings_dev.py' in the directory containing %r. You might want to adjust your DJANGO_SETTINGS_MODULE environment variable.\n" % __file__)
 	sys.exit(1)
 
 if __name__ == "__main__":
+	from django.core.management import execute_manager
 	execute_manager(settings_dev)
