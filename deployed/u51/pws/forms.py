ca001db6a8e3b3bbdc4da673dfa84405c4536dff
Niklas Hambüchen <nh2@deditus.de>
2010-09-13 20:31:45 +0200

    Added autocomplete="off" to the pw field

--- deployed/u51/pws/forms.py
+++ deployed/u51/pws/forms.py
@@ -12,7 +12,7 @@ class EintragForm(forms.ModelForm):
 	def __init__(self, *args, **kwargs):
 		super(EintragForm, self).__init__(*args, **kwargs)
 		self.fields['extra'].widget.attrs = {'cols': 24, 'rows': 5}
-		self.fields['pw'].widget = forms.PasswordInput(render_value=True)
+		self.fields['pw'].widget = forms.PasswordInput(render_value=True, attrs={'autocomplete': 'off'})
 
 	submit_name = 'create_edit_eintrag'
 
