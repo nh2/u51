25b59503019063eb2da834e6f100184f7f9d9c9a
Niklas Hambüchen <nh2@deditus.de>
2010-09-15 15:30:03 +0200

    Manage and sys.path cleanup

--- deployed/u51/manage.py
+++ deployed/u51/manage.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
+try:
+	import django
+except ImportError:
+	import sys
+	sys.stderr.write("Error: Can't import Django. Make sure that it is on sys.path or specify PYTHONPATH=/path/to/django on shell invocation.\n")
+	sys.exit(1)
+
 from django.core.management import execute_manager
 
 try:
-	import settings # Assumed to be in the same directory.
+	import settings
 except ImportError:
 	import sys
 	sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
--- deployed/u51/settings_dev.py
+++ deployed/u51/settings_dev.py
@@ -1,9 +1,6 @@
 from settings import *
 import sys, os, user
 
-# Appending django do the end so that you can use another version via PYTHONPATH
-sys.path.append(os.path.join(user.home,"src","django","django-1.2.x"))
-
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
