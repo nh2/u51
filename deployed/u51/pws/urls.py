ebfc4c3f4392e5579cf66b1e384dc3e4c198784e
Niklas Hambüchen <nh2@deditus.de>
2010-09-16 20:04:34 +0200

    Moved pws urls to own file

---
+++ deployed/u51/pws/urls.py
@@ -0,0 +1,27 @@
+from django.conf.urls.defaults import *
+from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.views.generic import list_detail
+from u51.pws.models import Entry
+from u51.pws import views, forms
+
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+	(r'^$', views.main, {}, 'main'),
+	(r'^edit/$', views.update_entry, {}, 'update'),
+	(r'^edit/(?P<id>\d+)/$', views.update_entry, {}, 'edit'),
+	(r'^delete/$', views.delete_entry, {}, 'delete'),
+)
+
+if settings.DEBUG:
+	urlpatterns += patterns('',
+		(r'^test/pwlist/$', list_detail.object_list, {'queryset': Entry.objects.all(), 'template_object_name': 'entry'}, 'test-pwlist'),
+
+		(r'^test/edit/$', views.update_entry, {'next': 'test-update', 'template': 'pws/pwform.html'}, 'test-update'),
+		(r'^test/edit/(?P<id>\d+)/$', views.update_entry, {'next': 'test-update', 'template': 'pws/pwform.html'}, 'test-edit'),
+
+		(r'^test/edit/flat/$', views.update_entry, {'next': 'test-update-flat', 'template': 'pws/pwform-flat.html'}, 'test-update-flat'),
+		(r'^test/edit/(?P<id>\d+)/flat/$', views.update_entry, {'next': 'test-update-flat', 'template': 'pws/pwform-flat.html'}, 'test-edit-flat'),
+	)
+
--- deployed/u51/urls.py
+++ deployed/u51/urls.py
@@ -1,27 +1,14 @@
-import os
 from django.conf.urls.defaults import *
 from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.views.generic import list_detail
 from django.contrib.auth.views import login, logout_then_login, password_change, password_change_done
-from u51.pws.models import Entry
-from u51.pws import views, forms
+from u51.pws import forms
 
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-	(r'^$', views.main, {}, 'main'),
-	(r'^edit/$', views.update_entry, {}, 'update'),
-	(r'^edit/(?P<id>\d+)/$', views.update_entry, {}, 'edit'),
-	(r'^delete/$', views.delete_entry, {}, 'delete'),
-
-	(r'^test/pwlist/$', list_detail.object_list, {'queryset': Entry.objects.all(), 'template_object_name': 'entry'}, 'test-pwlist'),
-
-	(r'^test/edit/$', views.update_entry, {'next': 'test-update', 'template': 'pws/pwform.html'}, 'test-update'),
-	(r'^test/edit/(?P<id>\d+)/$', views.update_entry, {'next': 'test-update', 'template': 'pws/pwform.html'}, 'test-edit'),
-
-	(r'^test/edit/flat/$', views.update_entry, {'next': 'test-update-flat', 'template': 'pws/pwform-flat.html'}, 'test-update-flat'),
-	(r'^test/edit/(?P<id>\d+)/flat/$', views.update_entry, {'next': 'test-update-flat', 'template': 'pws/pwform-flat.html'}, 'test-edit-flat'),
+	(r'^', include('u51.pws.urls')),
 
 	(r'^login/$', login, {'template_name': 'pws/login.html', 'authentication_form': forms.LoginForm}, 'login'),
 	(r'^logout/$', logout_then_login, {}, 'logout'),
@@ -31,6 +18,7 @@ urlpatterns = patterns('',
 
 if settings.DEBUG:
 	from django.views.static import serve
+	import os
 	urlpatterns += patterns('',
 		(r'^media/(.*)$', serve, {'document_root': os.path.join(settings.PROJECT_ROOT, '..', 'media')}),
 	)
