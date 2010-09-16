8129ed57482edab3e256f5fcd6519b4224b219d4
Niklas Hambüchen <nh2@deditus.de>
2010-09-16 18:54:02 +0200

    Fixed test URLs

--- deployed/u51/urls.py
+++ deployed/u51/urls.py
@@ -9,24 +9,19 @@ from u51.pws import views, forms
 
 # admin.autodiscover()
 
-eintraege = {
-	'queryset': Entry.objects.all(),
-	'template_object_name': 'entry',
-}
-
 urlpatterns = patterns('',
 	(r'^$', views.main, {}, 'main'),
 	(r'^edit/$', views.update_entry, {}, 'update'),
 	(r'^edit/(?P<id>\d+)/$', views.update_entry, {}, 'edit'),
 	(r'^delete/$', views.delete_entry, {}, 'delete'),
 
-	(r'^test/pwlist/$', list_detail.object_list, eintraege, 'pwlist'),
+	(r'^test/pwlist/$', list_detail.object_list, {'queryset': Entry.objects.all(), 'template_object_name': 'entry'}, 'test-pwlist'),
 
-	(r'^test/edit/$', views.update_entry, {'next': 'test-edit', 'template': 'pws/pwform.html'}, 'test-update'),
-	(r'^test/edit/(?P<id>\d+)/$', views.update_entry, {'next': 'test-edit', 'template': 'pws/pwform.html'}, 'test-edit'),
+	(r'^test/edit/$', views.update_entry, {'next': 'test-update', 'template': 'pws/pwform.html'}, 'test-update'),
+	(r'^test/edit/(?P<id>\d+)/$', views.update_entry, {'next': 'test-update', 'template': 'pws/pwform.html'}, 'test-edit'),
 
-	(r'^test/edit/flat/$', views.update_entry, {'next': 'test-edit-flat', 'template': 'pws/pwform-flat.html'}, 'test-update-flat'),
-	(r'^test/edit/(?P<id>\d+)/flat/$', views.update_entry, {'next': 'test-edit-flat', 'template': 'pws/pwform-flat.html'}, 'test-edit-flat'),
+	(r'^test/edit/flat/$', views.update_entry, {'next': 'test-update-flat', 'template': 'pws/pwform-flat.html'}, 'test-update-flat'),
+	(r'^test/edit/(?P<id>\d+)/flat/$', views.update_entry, {'next': 'test-update-flat', 'template': 'pws/pwform-flat.html'}, 'test-edit-flat'),
 
 	(r'^login/$', login, {'template_name': 'pws/login.html', 'authentication_form': forms.LoginForm}, 'login'),
 	(r'^logout/$', logout_then_login, {}, 'logout'),
