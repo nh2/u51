40ae7e7dfd84fc1ad31d559d9e2353f4d5d9d3ff
Niklas Hambüchen <nh2@deditus.de>
2010-09-17 18:17:02 +0200

    UI overhaul

--- deployed/u51/pws/forms.py
+++ deployed/u51/pws/forms.py
@@ -10,7 +10,7 @@ class EntryForm(forms.ModelForm):
 
 	def __init__(self, *args, **kwargs):
 		super(EntryForm, self).__init__(*args, **kwargs)
-		self.fields['extra'].widget.attrs = {'cols': 24, 'rows': 5}
+		self.fields['extra'].widget.attrs = {'cols': 20, 'rows': 5}
 		self.fields['pw'].widget = forms.PasswordInput(render_value=True, attrs={'autocomplete': 'off'})
 
 	submit_name = 'update_entry'
