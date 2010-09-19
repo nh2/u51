292a301285e6cf2e3725dce96008ceb3281b80cf
Niklas Hambüchen <nh2@deditus.de>
2010-09-19 02:17:09 +0200

    Whitespace

--- pws/models.py
+++ pws/models.py
@@ -5,7 +5,7 @@ class Entry(models.Model):
 	name = models.CharField(_('Where'), max_length=200,unique=True)
 	user = models.CharField(_('User'), max_length=200)
 	pw = models.CharField(_('Password'), max_length=200)
-	email = models.EmailField(_('Email'), )
+	email = models.EmailField(_('Email'))
 	extra = models.TextField(_('Extra info'), blank=True)
 	created = models.DateField(_('Created'), auto_now=True)
 	modified = models.DateField(_('Changed'), auto_now_add=True)
