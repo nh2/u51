553dcbe785a7259e3206f1633cdc55d5db27c54d
Niklas Hambüchen <mail@nh2.me>
2012-03-25 16:14:58 +0100

    Allow empty user, pw, email fields

--- pws/models.py
+++ pws/models.py
@@ -3,9 +3,9 @@ from django.utils.translation import ugettext_lazy as _
 
 class Entry(models.Model):
 	name = models.CharField(_('Where'), max_length=200, unique=True)
-	user = models.CharField(_('User'), max_length=200)
-	pw = models.CharField(_('Password'), max_length=200)
-	email = models.EmailField(_('Email'))
+	user = models.CharField(_('User'), max_length=200, blank=True)
+	pw = models.CharField(_('Password'), max_length=200, blank=True)
+	email = models.EmailField(_('Email'), blank=True)
 	extra = models.TextField(_('Extra info'), blank=True)
 	created = models.DateField(_('Created'), auto_now=True)
 	modified = models.DateField(_('Changed'), auto_now_add=True)
