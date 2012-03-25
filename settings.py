d74aab29892fc49b8e8dd75e6fa386f3e6cf5c42
Niklas Hambüchen <mail@nh2.me>
2012-03-25 16:09:04 +0100

    Add LOCALE_PATHS setting to avoid deprecation warning

--- settings.py
+++ settings.py
@@ -45,6 +45,10 @@ USE_I18N = True
 # calendars according to the current locale.
 USE_L10N = True
 
+LOCALE_PATHS = (
+	os.path.join(PROJECT_ROOT, 'locale'),
+)
+
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
