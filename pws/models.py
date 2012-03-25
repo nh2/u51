74a03c71133cf6c557b08b97297f223a9ef52d9a
Niklas Hambüchen <mail@nh2.me>
2012-03-25 16:14:28 +0100

    Whitespace

--- pws/models.py
+++ pws/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 class Entry(models.Model):
-	name = models.CharField(_('Where'), max_length=200,unique=True)
+	name = models.CharField(_('Where'), max_length=200, unique=True)
 	user = models.CharField(_('User'), max_length=200)
 	pw = models.CharField(_('Password'), max_length=200)
 	email = models.EmailField(_('Email'))
