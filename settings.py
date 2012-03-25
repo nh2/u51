b7006acc7f5f2705cd56fa5fb5aaeb28bc484526
Niklas Hambüchen <mail@nh2.me>
2012-03-25 16:01:57 +0100

    Fix database setting for postgresql

--- settings.py
+++ settings.py
@@ -13,9 +13,9 @@ MANAGERS = ADMINS
 
 DATABASES = {
 	'default': {
-		'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-		'NAME': '',                      # Or path to database file if using sqlite3.
-		'USER': '',                      # Not used with sqlite3.
+		'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+		'NAME': 'u51',                      # Or path to database file if using sqlite3.
+		'USER': 'u51',                      # Not used with sqlite3.
 		'PASSWORD': '',                  # Not used with sqlite3.
 		'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
 		'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
