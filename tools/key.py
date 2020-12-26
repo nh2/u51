4651fb99f8e4ad406a2814e01bd973c54ced98bb
Niklas Hambüchen <mail@nh2.me>
2020-12-27 00:16:01 +0100

    tools/key.py: Fix missing `raise`.
    Also:
    
    * Prompt file closing by using `Path().read/write_text()`.
    * Handle only relevant exceptions, don't discard underlying error messages.
    * Switch secret key generation to Django's helper function.

--- tools/key.py
+++ tools/key.py
@@ -1,15 +1,20 @@
+import errno
 import os
+from pathlib import Path
 
-def get_generate_secret_key(root, key_file_name='secret.key', length=50):
+from django.core.management.utils import get_random_secret_key
+
+def get_generate_secret_key(root, key_file_name='secret.key'):
 	key_path = os.path.join(root, key_file_name)
 	try:
-		return open(key_path).read().strip()
-	except IOError:
+		return Path(key_path).read_text().strip()
+	except IOError as ioe:
+		if ioe.errno not in [errno.ENOENT]:
+			raise
+		secret_key = get_random_secret_key()
 		try:
-			from random import choice
-			secret_key = ''.join([choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)') for i in range(length)])
-			with open(key_path, 'w') as key_file:
-				key_file.write(secret_key)
-			return secret_key
-		except IOError:
-			Exception('Could not generate a secret key file in %s. Please create it manually!' % key_path)
+			Path(key_path).write_text(secret_key)
+		except IOError as e:
+			detail_exception = Exception('Could not generate a secret key file in %s. Please create it manually!' % key_path)
+			raise detail_exception from e
+		return secret_key
